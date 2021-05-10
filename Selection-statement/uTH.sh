fa2c56d8d981f90f0d0f8c13273117ea38dda8fe
Srikar-coder <61378751+Srikar-coder@users.noreply.github.com>
2021-05-10 10:49:29 +0530

    Update uTH.sh

--- Selection-statement/uTH.sh
+++ Selection-statement/uTH.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -x
 read -p "Enter number" num
-dividend=$num
-divisor=10
-
-remainder=$((dividend%divisor))
-echo "unit: $remainder"
-
-dividend=$(($num/10))
-remainder=$((dividend%divisor))
-echo "ten: $remainder"
-
-dividend=$(($dividend/10))
-remainder=$((dividend%divisor))
-echo "hundred: $remainder"
+if [ $num -eq 1 ]
+echo unit
+elif [ $num -eq 10 ]
+echo ten
+elif [ $num -eq 100 ]
+echo hundred
+elif [ $num -eq 1000 ]
+echo thousand
+elif [ $num -eq 10000 ]
+echo ten thousand
+elif [ $num -eq 100000 ]
+echo lakh
+else
+echo out of range
+fi
